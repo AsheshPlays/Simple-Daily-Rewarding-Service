050f0fa20d847baf995bc2bfe677db19882d909f
Insthync <ittipon.bay@gmail.com>
2021-05-17 04:39:36 +0700

    Prepared configs, rewards, date data

--- index.ts
+++ index.ts
@@ -2,12 +2,32 @@ import fastify from 'fastify'
 import authPlugin from 'fastify-auth'
 import bearerAuthPlugin from 'fastify-bearer-auth'
 import { PrismaClient } from '@prisma/client'
-import * as dotenv from "dotenv"
+import * as dotenv from 'dotenv'
+import dateUtils from './utils/date'
+import data from './data.json'
 
 const prisma = new PrismaClient()
 dotenv.config()
 
 const passphrases: string = process.env['PASSPHRASES']!
+const mode = Number(process.env['MODE'])
+const days = mode == 0 ? 7 : 28
+const currentDate = new Date()
+const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth())
+const firstDayOfWeek = dateUtils.getMonday(currentDate)
+
+const rewards = []
+let j = 0
+for (let i = 0; i < days; ++i) {
+    if (j < data.length) {
+        rewards.push(data[j])
+    }
+    if (j + 1 >= data.length) {
+        j = 0
+    } else {
+        j++
+    }
+}
 
 const server = fastify()
     .register(authPlugin)
@@ -17,7 +37,17 @@ const server = fastify()
     })
     .after(() => {
 
-        
+        server.get('/:ownerId', async (request, reply) => {
+            // Get reward list and earn state
+        })
+
+        server.post('/claim/:ownerId', {
+            preHandler: server.auth([
+                server.verifyBearerAuth!
+            ]),
+        }, async (request, reply) => {
+            // Claim reward
+        })
     })
 
 
