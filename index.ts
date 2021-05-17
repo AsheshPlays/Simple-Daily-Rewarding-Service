538b1565c7c210a7c912951c31790976df70bca9
Insthync <ittipon.bay@gmail.com>
2021-05-17 11:14:38 +0700

    Implement get daily reward list function

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import authPlugin from 'fastify-auth'
 import bearerAuthPlugin from 'fastify-bearer-auth'
 import { PrismaClient } from '@prisma/client'
 import * as dotenv from 'dotenv'
-import dateUtils from './utils/date'
+import { DateTime } from 'luxon';
 import data from './data.json'
 
 const prisma = new PrismaClient()
@@ -11,12 +11,20 @@ dotenv.config()
 
 const passphrases: string = process.env['PASSPHRASES']!
 const mode = Number(process.env['MODE'])
+const consecutive = Boolean(Number(process.env['CONSECUTIVE']))
 const days = mode == 0 ? 7 : 28
-const currentDate = new Date()
-const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth())
-const firstDayOfWeek = dateUtils.getMonday(currentDate)
+const currentDate = DateTime.local()
+const startOfCurrentDate = DateTime.local().startOf('day')
+const endOfCurrentDate = DateTime.local().endOf('day')
+const startOfMonth = DateTime.local().startOf('month')
+const endOfMonth = DateTime.local().endOf('month');
+const startOfWeek = DateTime.local().startOf('week').minus({days:1})
+const endOfWeek = DateTime.local().endOf('week').minus({days:1})
+const currentDayOfWeek = DateTime.local().weekday
+const cycleStart = mode == 0 ? startOfWeek : startOfMonth
+const cycleEnd = mode == 0 ? endOfWeek : endOfMonth
 
-const rewards = []
+const rewards : any[] = []
 let j = 0
 for (let i = 0; i < days; ++i) {
     if (j < data.length) {
@@ -37,11 +45,73 @@ const server = fastify()
     })
     .after(() => {
 
-        server.get('/:ownerId', async (request, reply) => {
+        server.get('/:earnerId', async (request, reply) => {
+            const params : any = request.params
+            const earnerId = params.earnerId
             // Get reward list and earn state
+            const entries = await prisma.givenRewards.findMany({
+                where: {
+                    earnerId: earnerId,
+                    OR: [{
+                        createdAt: {
+                            gte: cycleStart.toJSDate(),
+                            lte: cycleEnd.toJSDate()
+                        }
+                    }],
+                },
+                orderBy: [{
+                    createdAt: 'asc'
+                }]
+            })
+            const count = entries.length
+            const result : any[] = []
+            let claimableDate = cycleStart
+            let foundClaimableEntry = false
+            for (let i = 0; i < rewards.length; i++) {
+                const reward = rewards[i]
+                let isClaimed = false
+                if (!consecutive) {
+                    for (let j = 0; j < entries.length; j++) {
+                        if (entries[j].createdAt.getTime() >= startOfCurrentDate.toJSDate().getTime() &&
+                            entries[j].createdAt.getTime() <= endOfCurrentDate.toJSDate().getTime() &&
+                            entries[j].createdAt.getTime() >= claimableDate.toJSDate().getTime() &&
+                            entries[j].createdAt.getTime() <= claimableDate.plus({days:1}).toJSDate().getTime())
+                        {
+                            isClaimed = true
+                            break;
+                        }
+                    }
+                } else {
+                    isClaimed = i < count
+                }
+                let canClaim = false
+                if (!isClaimed) {
+                    if (count > 0) {
+                        canClaim = entries[entries.length - 1].createdAt.getTime() < startOfCurrentDate.toJSDate().getTime()
+                    } else {
+                        canClaim = true
+                    }
+                    if (!consecutive) {
+                        canClaim = canClaim && currentDate.toJSDate().getTime() >= claimableDate.toJSDate().getTime()
+                        canClaim = canClaim && currentDate.toJSDate().getTime() <= claimableDate.plus({days:1}).toJSDate().getTime()
+                    } else {
+                        canClaim = canClaim && !foundClaimableEntry
+                    }
+                    if (!foundClaimableEntry) {
+                        foundClaimableEntry = canClaim
+                    }
+                }
+                result.push({
+                    isClaimed: isClaimed,
+                    canClaim: canClaim,
+                    reward: reward
+                })
+                claimableDate = claimableDate.plus({days:1})
+            }
+            reply.code(200).send(result)
         })
 
-        server.post('/claim/:ownerId', {
+        server.post('/claim/:earnerId', {
             preHandler: server.auth([
                 server.verifyBearerAuth!
             ]),
