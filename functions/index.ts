242b7e0a83ca51738a952fabc90ce3b169a06e26
Insthync <ittipon.bay@gmail.com>
2021-05-18 01:13:12 +0700

    Set parameter's types

--- functions/index.ts
+++ functions/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { IClaimableRewards } from '../interfaces'
 
 export default {
-    sendRewards: async function(prisma : PrismaClient, IClaimableRewards : any) : Promise<boolean> {
+    sendRewards: async function(prisma : PrismaClient, claimableRewards : IClaimableRewards) : Promise<boolean> {
         // NOTE: Fork this repository and modify this function to send rewards
         return false
     }
