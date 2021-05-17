b0f6724162631efd3ff339feaaee9d107f764df5
Insthync <ittipon.bay@gmail.com>
2021-05-17 11:14:16 +0700

    Change to use `luxon` for date time tools

--- utils/date/index.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    getMonday: function(d: Date) {
-        d = new Date(d)
-        const day = d.getDay()
-        const diff = d.getDate() - day + (day == 0 ? -6 : 1) // adjust when day is sunday
-        d = new Date(d.setDate(diff))
-        return new Date(d.getFullYear(), d.getMonth(), d.getDate())
-    }
-}
