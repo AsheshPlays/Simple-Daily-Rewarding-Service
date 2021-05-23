e377ed19667fa7e513cca15445646b45f60a21b0
Insthync <ittipon.bay@gmail.com>
2021-05-24 01:28:04 +0700

    Change default result

--- functions/index.ts
+++ functions/index.ts
@@ -4,6 +4,6 @@ import { IClaimableRewards } from '../interfaces'
 export default {
     sendRewards: async function(prisma : PrismaClient, claimableRewards : IClaimableRewards) : Promise<boolean> {
         // NOTE: Fork this repository and modify this function to send rewards
-        return false
+        return true
     }
 }
